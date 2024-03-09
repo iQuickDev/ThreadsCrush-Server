42beeb92dab9624bf7d575d807ba1540960e019f
iQuick <iquickpublic@gmail.com>
2024-03-09 03:11:05 +0100

    do not crash if no .env is found

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,11 @@ use dotenvy::dotenv;
 use loco_rs::cli;
 use migration::Migrator;
 use threads_crush::app::App;
+use tracing::warn;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    dotenv().expect("Failed to load .env file");
+    dotenv().map_err(|e| warn!("Error loading .env file: {}", e)).ok();
     cli::main::<App, Migrator>().await
 }
