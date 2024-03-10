2e2c90b741d5c2585856e6394db6b54b330dd814
iQuick <iquickpublic@gmail.com>
2024-03-11 00:46:53 +0100

    remove ip logs and final push thanks

--- src/controllers/vote/vote.rs
+++ src/controllers/vote/vote.rs
@@ -122,14 +122,11 @@ pub async fn vote(
 /// Get the IP address from the request headers (railway.app includes the real
 /// IP in the "x-Envoy-external-Address" or "x-forwarded-for" headers)
 fn get_ip(secure_ip: &SecureClientIp, headers: &HeaderMap) -> String {
-    debug!("headers: {:?}", headers);
-
     if let Some(ip) = headers
         .get("x-Envoy-external-Address")
         .map(|header| header.to_str().ok())
         .flatten()
     {
-        debug!("using x-Envoy-external-Address: {}", ip);
         return ip.to_string();
     }
 
@@ -140,12 +137,10 @@ fn get_ip(secure_ip: &SecureClientIp, headers: &HeaderMap) -> String {
         .map(|header| header.split(',').last())
         .flatten()
     {
-        debug!("using x-forwarded-for: {}", ip);
         return ip.to_string();
     }
 
     let layer_ip = secure_ip.0.to_canonical().to_string();
-    debug!("using secure_ip: {:?}", layer_ip);
     layer_ip
 }
 
